875d9ba13b0e0d9f308db5c1eeab211729509a64
XIAO <xiaoheroine@gmail.com>
2022-06-18 13:26:51 +0800

    Updated refresher.py

--- Refresher.py
+++ Refresher.py
@@ -1,1 +1,22 @@
 # Learning python is essential for the DevOps role.
+
+from asyncio import tasks
+
+from pyparsing import InfixNotationOperatorSpec
+
+
+Usage of python
+1. Web Development
+2. Data Science
+3. Machine learning
+4. Artificial intelligence
+5. Web Scraping 
+6. Automation
+    - Automate DevOps tasks
+    > CI/CD Pipelines
+    > Cloud platforms
+    > Monitoring Infrastructure
+    > Automate Backups and Cleanups of servers 
+    > Automate General tasks, excel sheets
+
+
